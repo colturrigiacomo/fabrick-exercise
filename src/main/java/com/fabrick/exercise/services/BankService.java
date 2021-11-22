0f774cb1a246b82efecee9e02b3590ffb80560ab
giacomocolturri <colturri@omicronconsulting.it>
2021-11-22 14:22:14 +0100

    Change interface method return class

--- src/main/java/com/fabrick/exercise/services/BankService.java
+++ src/main/java/com/fabrick/exercise/services/BankService.java
@@ -1,15 +1,18 @@
 package com.fabrick.exercise.services;
 
 import com.fabrick.exercise.models.Balance;
+import com.fabrick.exercise.models.PaymentResponse;
+import com.fabrick.exercise.models.Transaction;
 import com.fabrick.exercise.models.requests.MoneyTransfer;
 
 import java.util.Date;
+import java.util.List;
 
 public interface BankService {
 
     Balance getBalance(String accountId);
 
-    Object getTransactions(String accountId, Date dateFrom, Date dateTo);
+    List<Transaction> getTransactions(String accountId, Date dateFrom, Date dateTo);
 
-    Object createMoneyTransfer(MoneyTransfer req, String accountId);
+    PaymentResponse createMoneyTransfer(MoneyTransfer req, String accountId);
 }
